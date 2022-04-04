5ac3b48be38ab8002fc67d8d15f3d7812dc44cac
Amieva, Abel Aron <abelamieva@gmail.com>
2022-04-04 09:46:10 -0300

    variables de entorno cambiadas

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { MapsApp } from './MapsApp';
-import 'dotenv/config';
+// import 'dotenv/config';
  
-mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN || '';
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN || '';
 
 if( !navigator.geolocation ){
   alert('Tu navegador no tiene geolocation');
